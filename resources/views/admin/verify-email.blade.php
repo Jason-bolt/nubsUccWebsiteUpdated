1149ae84a2fdf75b172fdcd609cb4da8fd2fe32f
Jason-bolt <appiatujason@gmail.com>
2022-02-22 11:22:49 +0000

    Auth Redesign

---
+++ resources/views/admin/verify-email.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Register</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        crossorigin="anonymous"
+    />
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"
+    ></script>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css"
+        integrity="sha384-7ynz3n3tAGNUYFZD3cWe5PDcE36xj85vyFkawcF6tIwxvIecqKvfwLiaFdizhPpN"
+        crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="../css/css.css" />
+</head>
+<body>
+<section class="p-sm-5">
+    <div class="container mt-5">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <img
+                    src="../assets/nubs_logo.png"
+                    alt="NUBS LOGO"
+                    class="img-fluid"
+                    width="700"
+                />
+            </div>
+
+            <div class="col-md-6 m-auto">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <p class="text-secondary">
+                            You are now signing up! Before getting started, could you
+                            verify your email address by clicking on the link we just
+                            emailed to you? If you didn't receive the email, another one
+                            will be sent to you.
+                        </p>
+                        <form>
+                            <div class="row">
+                                <div class="col">
+                                    <button class="btn btn-nubsBlue mt-4">
+                                        Resend Verification Email
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        <form action="#">
+                            <button class="btn text-secondary mt-4">Logout</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
