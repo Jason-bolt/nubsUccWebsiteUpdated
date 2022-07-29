d2b5988230d146ef76a62da912b684d9a53e0292
Jason-bolt <appiatujason@gmail.com>
2022-07-29 16:57:08 +0000

    Added a location model

---
+++ app/Models/Location.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'location'
+    ];
+
+    /*
+     * Get the activity(ies) that have this location
+     * */
+    public function activities()
+    {
+        return $this->belongsToMany(WeeklyActivity::class);
+    }
+
+}
