d42842009ca57c4349f29bf480d8f6bef6a5405f
Jason-bolt <appiatujason@gmail.com>
2022-08-01 02:50:52 +0000

    Made image nullable

--- database/migrations/2022_07_31_194444_create_testimonies_table.php
+++ database/migrations/2022_07_31_194444_create_testimonies_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('testimonies', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->string('name');
             $table->string('testimony');
             $table->boolean('is_accepted');
