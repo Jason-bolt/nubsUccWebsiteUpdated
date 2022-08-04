a6c9edab762ab8ef780b9591ec34670071bef01e
Jason-bolt <appiatujason@gmail.com>
2022-08-04 03:26:54 +0000

    Added add executive button and New button for adding a new year group

--- resources/views/cms/executives.blade.php
+++ resources/views/cms/executives.blade.php
@@ -8,6 +8,83 @@
         </div>
     </section>
 
+    <section class="container mb-5 mb-sm-0">
+        {{-- Button to add an executive --}}
+        <button class="btn btn-nubsBlue" data-bs-toggle="modal" data-bs-target="#add_executive">
+            Add an executive <i class="bi bi-plus-lg"></i>
+        </button>
+
+        {{-- Modal to add new executive --}}
+        <div class="modal fade mt-0" id="add_executive" tabindex="-1">
+            <div class="modal-dialog modal-fullscreen-md-down">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="my-0">Add Executive</h5>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="" method="POST" enctype="multipart/form-data">
+                            <div class="form-group mb-3">
+                                <label class="lead" for="image"
+                                >Executive image</label
+                                >
+                                <input
+                                    type="file"
+                                    class="form-control"
+                                    name="image"
+                                    id="image"
+                                />
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label class="lead" for="name">Name</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="James Doe" required />
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label class="lead" for="program"
+                                >Program</label
+                                >
+                                <input type="text" id="program" name="program" class="form-control" placeholder="B.Sc. Computer Science" required>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label class="lead" for="contact"
+                                >Contact</label
+                                >
+                                <input type="text" id="contact" name="contact" class="form-control" placeholder="0123456789 / 0123456789" required>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label class="lead" for="office"
+                                >Office</label
+                                >
+                                <input type="text" id="office" name="office" class="form-control" placeholder="Music Wing President" required>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label class="lead" for="year_group"
+                                >Year group</label
+                                >
+                                <select name="year_group" id="year_group" class="form-control">
+                                    <option value="1">2019/20</option>
+                                </select>
+                            </div>
+
+                            <button class="btn btn-nubsBlue">Save changes</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
     <!-- Executives Per Batch -->
     <section class="p-sm-5">
         <div class="container">
@@ -21,6 +98,12 @@
             <a href="#21_22" class="btn btn-nubsBlue rounded-pill px-3 shadow my-2"
             >2021/22</a
             >
+
+            {{-- Add a new year group --}}
+            <a href="#" class="btn btn-outline-secondary rounded-pill">
+                New <i class="bi bi-plus-lg"></i>
+            </a>
+
 
             <!-- Result of search -->
             <h2 class="my-5" id="#1920">2019/20 Batch of Executives</h2>
