84badb12602fe7fa0e520c5c9ad426e15607e051
Jason-bolt <appiatujason@gmail.com>
2022-10-30 16:20:58 +0000

    Added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+{{ $page = '' }}
+@extends('myLayout.app')
+
+@section('content')
+
+    <section class="py-5 my-5">
+        <div class="container py-5 my-5 text-center" style="color: rgb(3, 23, 83)">
+            <h1 class="fw-bold" style="font-size: 150px;">
+                404
+            </h1>
+            <p class="lead">Sorry, the page you are looking for cannot be found!</p>
+        </div>
+    </section>
+
+@endsection
